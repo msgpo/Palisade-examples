2103c4eb1d58f7709a52d1d7b97ccbcd12a281ea
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-14 15:43:35 +0100

    added namespace test to scripts

--- deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
+++ deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
@@ -4,12 +4,10 @@ helm dep up
 helm upgrade --install palisade . \
   --set global.persistence.classpathJars.local.hostPath=$(pwd)/deployment/target \
   --set global.persistence.dataStores.palisade-data-store.local.hostPath=$(pwd)/resources/data \
-  --set global.persistence.kafka.local.hostPath=$(pwd) \
   --set global.persistence.redisMaster.local.hostPath=$(pwd) \
   --set global.persistence.redisSlave.local.hostPath=$(pwd) \
   --set global.persistence.zookeeper.local.hostPath=$(pwd) \
   --set traefik.install=true \
-  --set kafka.install=true \
   --set redis.install=true \
   --set global.hosting=local \
   --set redis-cluster.install=false \
