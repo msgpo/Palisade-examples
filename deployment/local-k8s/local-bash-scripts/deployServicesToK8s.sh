e6d5718615a5f5bc5bf9f57926dd629d86cf4ddf
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-07-14 11:30:55 +0100

    Pal 544 updated k8s deploy script

--- deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
+++ deployment/local-k8s/local-bash-scripts/deployServicesToK8s.sh
@@ -1,4 +1,17 @@
 #! /usr/bin/env bash
 
 helm dep up
-helm upgrade --install palisade . --set global.persistence.dataStores.palisade-data-store.local.hostPath=$(pwd)/resources/data --set global.persistence.classpathJars.local.hostPath=$(pwd)/deployment/target --namespace test
+helm upgrade --install palisade . \
+  --set global.persistence.classpathJars.local.hostPath=$(pwd)/deployment/target \
+  --set global.persistence.dataStores.palisade-data-store.local.hostPath=$(pwd)/resources/data \
+  --set global.persistence.kafka.local.hostPath=$(pwd) \
+  --set global.persistence.redisMaster.local.hostPath=$(pwd) \
+  --set global.persistence.redisSlave.local.hostPath=$(pwd) \
+  --set global.persistence.zookeeper.local.hostPath=$(pwd) \
+  --set traefik.install=true \
+  --set kafka.install=true \
+  --set redis.install=true \
+  --set global.hosting=local \
+  --set redis-cluster.install=false \
+  --timeout=200s \
+  --namespace test
