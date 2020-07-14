fd4490bf24091cb3c678c58cf1ed4368fe38c1e9
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-14 16:04:54 +0100

    updated namespaces again

--- deployment/local-k8s/local-bash-scripts/verify.sh
+++ deployment/local-k8s/local-bash-scripts/verify.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-kubectl exec $(kubectl get pods --namespace test | awk '/example-model/ {print $1}') --namespace test -- bash -c "cd /usr/share/example-model && bash ./verify.sh"
+kubectl exec $(kubectl get pods --namespace pal-544-ad | awk '/example-model/ {print $1}') --namespace pal-544-ad -- bash -c "cd /usr/share/example-model && bash ./verify.sh"
