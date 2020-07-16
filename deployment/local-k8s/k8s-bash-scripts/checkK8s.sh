785177e5acf875c97f769367bce69043858647f8
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-16 16:00:34 +0100

    Updated checkK8s Script

--- deployment/local-k8s/k8s-bash-scripts/checkK8s.sh
+++ deployment/local-k8s/k8s-bash-scripts/checkK8s.sh
@@ -9,3 +9,8 @@ helm uninstall palisade -n "$1"
 kubectl delete pods -n "$1" --all
 kubectl delete jobs -n "$1" --all
 kubectl delete namespaces "$1"
+
+kubectl get pods -n "$1"
+kubectl get pv -n "$1"
+kubectl get pvc -n "$1"
+kubectl get jobs -n "$1"
