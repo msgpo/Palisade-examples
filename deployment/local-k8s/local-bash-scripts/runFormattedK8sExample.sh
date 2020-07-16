72b0ec1e2e59500eb90cf9c0d00e1e87c5fe3d03
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-16 19:18:07 +0100

    updated scripts to accept namespace

--- deployment/local-k8s/local-bash-scripts/runFormattedK8sExample.sh
+++ deployment/local-k8s/local-bash-scripts/runFormattedK8sExample.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-kubectl exec $(kubectl get pods --namespace pal-544-ad | awk '/example-model/ {print $1}') --namespace pal-544-ad -- bash -c "cd /usr/share/example-model && bash ./runFormattedK8sExample.sh"
+kubectl exec $(kubectl get pods --namespace "$1" | awk '/example-model/ {print $1}') --namespace "$1" -- bash -c "cd /usr/share/example-model && bash ./runFormattedK8sExample.sh"
