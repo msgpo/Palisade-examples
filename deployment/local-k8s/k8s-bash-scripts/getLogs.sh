3e76e4f9ad741aa93cf02f2ea4dffbd2aeac18bc
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-16 19:15:00 +0100

    updated getLogs

--- deployment/local-k8s/k8s-bash-scripts/getLogs.sh
+++ deployment/local-k8s/k8s-bash-scripts/getLogs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sleep 60s
-kubectl --namespace $1 logs $(kubectl get pods --namespace $1 | awk '/'$2'/ {print $1}')
+kubectl logs --namespace=$1 $(kubectl get pods --namespace $1 | awk '/'$2'/ {print $1}')
