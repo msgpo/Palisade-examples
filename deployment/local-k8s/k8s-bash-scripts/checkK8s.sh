4b8d20237572ec495802dbd863cc900a38648410
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-16 17:47:40 +0100

    Updated check

--- deployment/local-k8s/k8s-bash-scripts/checkK8s.sh
+++ deployment/local-k8s/k8s-bash-scripts/checkK8s.sh
@@ -5,10 +5,15 @@ kubectl get pods -n "$1"
 kubectl get pv -n "$1"
 kubectl get pvc -n "$1"
 kubectl get jobs -n "$1"
+
+
 helm uninstall palisade -n "$1"
 kubectl delete pods -n "$1" --all
 kubectl delete jobs -n "$1" --all
+kubectl delete pv palisade-data-store-pal-544-ad
+kubectl delete pv palisade-classpath-jars-example-pal-544-ad
 kubectl delete namespaces "$1"
+
 
 kubectl get pods -n "$1"
 kubectl get pv -n "$1"
